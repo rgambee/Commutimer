8fcfefcbb679c30aff538b1c7d70fffb0698441d
Robert Gambee <robertgambee@gmail.com>
2016-11-27 15:22:27 -0500

    Add spinner for selecting leg type

--- app/src/main/java/gambee/robert/commutimer/NewTripActivity.java
+++ app/src/main/java/gambee/robert/commutimer/NewTripActivity.java
@@ -11,10 +11,16 @@ public class NewTripActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip);
 
-        Spinner spinner = (Spinner) findViewById(R.id.trip_preset_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        Spinner presetSpinner = (Spinner) findViewById(R.id.trip_preset_spinner);
+        ArrayAdapter<CharSequence> presetAdapter = ArrayAdapter.createFromResource(this,
                 R.array.dummy_trip_presets, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        presetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        presetSpinner.setAdapter(presetAdapter);
+
+        Spinner leg1TypeSpinner = (Spinner) findViewById(R.id.leg1_type_spinner);
+        ArrayAdapter<CharSequence> leg1TypeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.leg_types, android.R.layout.simple_spinner_item);
+        leg1TypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        leg1TypeSpinner.setAdapter(leg1TypeAdapter);
     }
 }
