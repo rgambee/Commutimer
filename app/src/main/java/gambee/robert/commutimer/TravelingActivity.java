a6570633b8b61a12219e21b8d9681a705e7060b0
Robert Gambee <robertgambee@gmail.com>
2016-12-04 14:33:23 -0500

    Add timer to traveling activity

--- app/src/main/java/gambee/robert/commutimer/TravelingActivity.java
+++ app/src/main/java/gambee/robert/commutimer/TravelingActivity.java
@@ -2,11 +2,19 @@ package gambee.robert.commutimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
 
 public class TravelingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traveling);
+    }
+
+    public void beginTraveling(View view) {
+        long tripStartTime = System.currentTimeMillis();
+        Chronometer timer = (Chronometer) findViewById(R.id.trip_timer);
+        timer.start();
     }
 }
