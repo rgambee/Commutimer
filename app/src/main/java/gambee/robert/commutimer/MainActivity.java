d5cbd9a9d3242d7bdf3a14546be3224944b24650
Robert Gambee <robertgambee@gmail.com>
2016-12-04 14:09:49 -0500

    Remove unused imports

--- app/src/main/java/gambee/robert/commutimer/MainActivity.java
+++ app/src/main/java/gambee/robert/commutimer/MainActivity.java
@@ -1,14 +1,9 @@
 package gambee.robert.commutimer;
 
 import android.content.Intent;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-
-import java.io.File;
-import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "gambee.robert.commutimer.MESSAGE";
