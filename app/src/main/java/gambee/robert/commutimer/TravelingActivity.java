ebd7e2011f65537bf867a4b3a11ff46396983c99
Robert Gambee <robertgambee@gmail.com>
2017-08-27 19:59:53 -0400

    Update TravelingActivity's onCreate() method

--- app/src/main/java/gambee/robert/commutimer/TravelingActivity.java
+++ app/src/main/java/gambee/robert/commutimer/TravelingActivity.java
@@ -1,39 +1,41 @@
 package gambee.robert.commutimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Locale;
 
 public class TravelingActivity extends AppCompatActivity {
+    private LinearLayout legListLayout;
+    private ArrayList<Chronometer> legTimers = new ArrayList<Chronometer>(3);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traveling);
 
-        // TODO: link this to Trip from NewTripActivity
-        Trip dummyTrip = new Trip();
-        dummyTrip.addLeg(new TripLeg("Foot"));
-        dummyTrip.addLeg(new TripLeg("Bus"));
-        dummyTrip.addLeg(new TripLeg("Subway"));
-        dummyTrip.addLeg(new TripLeg("Other"));
+        Intent intent = getIntent();
+        Trip trip = (Trip) intent.getParcelableExtra("TripParcel");
 
-        LinearLayout legListLayout = (LinearLayout) findViewById(R.id.traveling_leg_list_layout);
-        legListLayout.setOrientation(LinearLayout.VERTICAL);
+        legListLayout = (LinearLayout) findViewById(R.id.traveling_leg_list_layout);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
-        legListLayout.setLayoutParams(params);
         int legNumber = 0;
-        Iterator<TripLeg> legIter = dummyTrip.iterLegs();
+        Iterator<TripLeg> legIter = trip.iterLegs();
         while (legIter.hasNext()) {
+            TripLeg t = legIter.next();
+            Log.d("CommutimerDebug", t.getLegType());
             LinearLayout legLayout = new LinearLayout(this);
             legLayout.setOrientation(LinearLayout.HORIZONTAL);
             legLayout.setLayoutParams(params);
@@ -41,15 +43,20 @@ public class TravelingActivity extends AppCompatActivity {
             TextView legLabel = new TextView(this);
             legLabel.setText(String.format(Locale.US, "Leg %d", legNumber));
             legLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Large);
-            ++legNumber;
 
             TextView legTypeLabel = new TextView(this);
-            legTypeLabel.setText(legIter.next().getLegType());
+            legTypeLabel.setText(t.getLegType());
             legTypeLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Menu);
+
+            Chronometer chron = new Chronometer(this);
+            legTimers.add(chron);
 
             legLayout.addView(legLabel);
             legLayout.addView(legTypeLabel);
+            legLayout.addView(chron);
             legListLayout.addView(legLayout);
+
+            ++legNumber;
         }
     }
 
