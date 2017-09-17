9b6b2aa2854ae475d6cda35dbbf8591a575a492e
Robert Gambee <robertgambee@gmail.com>
2017-09-17 11:27:16 -0400

    Expand GtfsInfo's content

--- app/src/main/java/gambee/robert/commutimer/GtfsInfo.java
+++ app/src/main/java/gambee/robert/commutimer/GtfsInfo.java
@@ -12,12 +12,39 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 public class GtfsInfo {
     private HashMap<String, ArrayList<String>> routesByMode = new HashMap<>();
+    private HashMap<String, HashMap<String, ArrayList<String>>> stopsByRoute = new HashMap<>();
 
     public GtfsInfo() {
         parseRoutes();
+    }
+
+    public ArrayList<String> getRoutesForMode(String mode) {
+        if (routesByMode.isEmpty()) {
+            parseRoutes();
+        }
+        return routesByMode.get(mode);
+    }
+
+    public Set<String> getDirectionsForRoute(String route) {
+        if (!stopsByRoute.containsKey(route)) {
+            if (!parseStops(route)) {
+                return null;
+            }
+        }
+        return stopsByRoute.get(route).keySet();
+    }
+
+    public ArrayList<String> getStopsForRouteDirection(String route, String direction) {
+        if (!stopsByRoute.containsKey(route)) {
+            if (!parseStops(route)) {
+                return null;
+            }
+        }
+        return stopsByRoute.get(route).get(direction);
     }
 
     private void parseRoutes() {
@@ -30,8 +57,8 @@ public class GtfsInfo {
             FileInputStream fis = new FileInputStream(routesFile);
             XmlPullParser parser = Xml.newPullParser();
             parser.setInput(fis, null);
-            parser.next();
-            while (parser.next() != XmlPullParser.END_TAG) {
+            parser.nextTag();
+            while (parser.nextTag() != XmlPullParser.END_TAG) {
                 if (parser.getEventType() == XmlPullParser.START_TAG) {
                     String name = parser.getName();
                     if (name.equals("mode")) {
@@ -60,7 +87,49 @@ public class GtfsInfo {
         }
     }
 
-    private void downloadRoutes() {
-        return;
+    private boolean parseStops(String route) {
+        File stopsFile = new File(new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), "RouteInfo"), route + ".xml");
+        if (!stopsFile.exists()) {
+            if (!downloadStops()) {
+                return false;
+            }
+        }
+        try {
+            FileInputStream fis = new FileInputStream(stopsFile);
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setInput(fis, null);
+            parser.nextTag();
+            while (parser.nextTag() != XmlPullParser.END_TAG) {
+                if (parser.getEventType() == XmlPullParser.START_TAG) {
+                    if (parser.getName().equals("direction")) {
+                        String direction = parser.getAttributeValue(null, "direction_name");
+                        ArrayList<String> stops = new ArrayList<>();
+                        while (parser.nextTag() != XmlPullParser.END_TAG) {
+                            stops.add(parser.getAttributeValue(null, "parent_station_name"));
+                        }
+                        if (stopsByRoute.containsKey(route)) {
+                            stopsByRoute.get(route).put(direction, stops);
+                        } else {
+                            HashMap<String, ArrayList<String>> stopsForDirection = new HashMap<>();
+                            stopsForDirection.put(direction, stops);
+                            stopsByRoute.put(route, stopsForDirection);
+                        }
+                    }
+                }
+            }
+        } catch (IOException | XmlPullParserException ex) {
+            Log.e("CommutimerError", ex.toString());
+            return false;
+        }
+        return true;
+    }
+
+    private boolean downloadRoutes() {
+        return false;
+    }
+
+    private boolean downloadStops() {
+        return false;
     }
 }
