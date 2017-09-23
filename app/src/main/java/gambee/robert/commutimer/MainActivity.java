69756fa0496ed9a21c9a567dddc7b0d4f822526b
Robert Gambee <robertgambee@gmail.com>
2017-09-23 13:16:48 -0400

    Update permissions and prompt user

--- app/src/main/java/gambee/robert/commutimer/MainActivity.java
+++ app/src/main/java/gambee/robert/commutimer/MainActivity.java
@@ -1,15 +1,37 @@
 package gambee.robert.commutimer;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE
+    };
+    private static final int PERMISSIONS_REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+            || checkSelfPermission(Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                PERMISSIONS,
+                PERMISSIONS_REQUEST_CODE
+            );
+        }
     }
 
     public void openNewTrip(View view) {
