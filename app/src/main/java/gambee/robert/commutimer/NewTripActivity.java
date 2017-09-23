894955375d8e256b260dd88ed2b739710ba4dd3e
Robert Gambee <robertgambee@gmail.com>
2017-09-23 18:53:00 -0400

    Add new spinners for selection trip leg details

--- app/src/main/java/gambee/robert/commutimer/NewTripActivity.java
+++ app/src/main/java/gambee/robert/commutimer/NewTripActivity.java
@@ -11,16 +11,22 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
 
 public class NewTripActivity extends AppCompatActivity {
     private int legNumber = 0;
     private LinearLayout legListLayout;
     private ArrayList<TripLeg> tripLegs = new ArrayList<TripLeg>(3);
+    private GtfsInfo routeInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        routeInfo =  new GtfsInfo(Volley.newRequestQueue(this));
+
         setContentView(R.layout.activity_new_trip);
 
         Spinner presetSpinner = (Spinner) findViewById(R.id.trip_preset_spinner);
@@ -34,34 +40,48 @@ public class NewTripActivity extends AppCompatActivity {
     }
 
     public void addNewLeg(View view) {
-        TextView legLabel = new TextView(this);
-        legLabel.setText(getString(R.string.leg_label, legNumber));
-        legLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Large);
-
         LinearLayout modeLayout = new LinearLayout(this);
+        LinearLayout routeLayout = new LinearLayout(this);
+        LinearLayout stopsLayout = new LinearLayout(this);
+
+        TextView legLabel = new TextView(this);
+        TextView modeLabel = new TextView(this);
+        TextView routeLabel = new TextView(this);
+        TextView directionLabel = new TextView(this);
+        TextView sourceLabel = new TextView(this);
+        TextView destinationLabel = new TextView(this);
+
+        final Spinner modeSpinner = new Spinner(this);
+        final Spinner routeSpinner = new Spinner(this);
+        final Spinner directionSpinner = new Spinner(this);
+        final Spinner sourceSpinner = new Spinner(this);
+        final Spinner destinationSpinner = new Spinner(this);
+
         modeLayout.setOrientation(LinearLayout.HORIZONTAL);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(getResources().getDimensionPixelOffset(R.dimen.activity_horizontal_margin),
-                          0, 0,
-                          getResources().getDimensionPixelOffset(R.dimen.activity_vertical_margin));
         modeLayout.setLayoutParams(params);
 
-        TextView modeLabel = new TextView(this);
+        legLabel.setText(getString(R.string.leg_label, legNumber));
+        legLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Large);
+
         modeLabel.setText(R.string.mode_label);
         modeLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Menu);
+        modeLabel.setPadding(
+                getResources().getDimensionPixelOffset(R.dimen.activity_horizontal_margin),
+                0, 0, 0);
 
-        final Spinner modeSpinner = new Spinner(this);
         ArrayAdapter<CharSequence> modeAdapter = ArrayAdapter.createFromResource(this,
-                R.array.leg_types, android.R.layout.simple_spinner_item);
-        modeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                R.array.leg_types, android.R.layout.simple_spinner_dropdown_item);
         modeSpinner.setAdapter(modeAdapter);
         modeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            private int legIndex = legNumber;
+            private final int legIndex = legNumber;
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                tripLegs.get(legIndex).setMode((String) modeSpinner.getSelectedItem());
+                String mode = (String) modeSpinner.getSelectedItem();
+                tripLegs.get(legIndex).setMode(mode);
+                updateRoutes(routeSpinner, mode);
             }
 
             @Override
@@ -69,13 +89,135 @@ public class NewTripActivity extends AppCompatActivity {
                 return;
             }
         });
+        modeLayout.addView(legLabel);
         modeLayout.addView(modeLabel);
         modeLayout.addView(modeSpinner);
 
-        legListLayout.addView(legLabel);
+        routeLabel.setText(R.string.route_label);
+        routeLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Menu);
+        directionLabel.setText(R.string.direction_label);
+        directionLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Menu);
+
+        routeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            private final int legIndex = legNumber;
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String route = (String) routeSpinner.getSelectedItem();
+                tripLegs.get(legIndex).setRoute(route);
+                updateDirections(directionSpinner, route);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+        directionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            private final int legIndex = legNumber;
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String direction = (String) directionSpinner.getSelectedItem();
+                tripLegs.get(legIndex).setRouteDirection(direction);
+                updateStops(sourceSpinner, tripLegs.get(legIndex).getRoute(), direction);
+                updateStops(destinationSpinner, tripLegs.get(legIndex).getRoute(), direction);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        routeLayout.setOrientation(LinearLayout.HORIZONTAL);
+        routeLayout.setLayoutParams(params);
+        routeLayout.addView(routeLabel);
+        routeLayout.addView(routeSpinner);
+        routeLayout.addView(directionLabel);
+        routeLayout.addView(directionSpinner);
+
+        sourceLabel.setText(R.string.source_label);
+        sourceLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Menu);
+        destinationLabel.setText(R.string.destination_label);
+        destinationLabel.setTextAppearance(R.style.Base_TextAppearance_AppCompat_Menu);
+
+        sourceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            private final int legIndex = legNumber;
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String source = (String) sourceSpinner.getSelectedItem();
+                tripLegs.get(legIndex).setSource(source);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+        destinationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            private final int legIndex = legNumber;
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String destination = (String) destinationSpinner.getSelectedItem();
+                tripLegs.get(legIndex).setDestination(destination);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        stopsLayout.setOrientation(LinearLayout.HORIZONTAL);
+        stopsLayout.setLayoutParams(params);
+        stopsLayout.addView(sourceLabel);
+        stopsLayout.addView(sourceSpinner);
+        stopsLayout.addView(destinationLabel);
+        stopsLayout.addView(destinationSpinner);
+
         legListLayout.addView(modeLayout);
+        legListLayout.addView(routeLayout);
+        legListLayout.addView(stopsLayout);
         tripLegs.add(new TripLeg());
         legNumber++;
+    }
+
+    private void updateRoutes(Spinner spinner, String mode) {
+        ArrayList<String> routes = routeInfo.getRoutesForMode(mode);
+        if (routes == null) {
+            routes = new ArrayList<>(0);
+            spinner.setEnabled(false);
+        } else {
+            spinner.setEnabled(true);
+        }
+        ArrayAdapter<String> routeAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item, routes);
+        spinner.setAdapter(routeAdapter);
+    }
+
+    private void updateDirections(Spinner spinner, String route) {
+        ArrayList<String> directions = routeInfo.getDirectionsForRoute(route);
+        if (directions == null) {
+            directions = new ArrayList<>(0);
+            spinner.setEnabled(false);
+        } else {
+            spinner.setEnabled(true);
+        }
+        ArrayAdapter<String> routeAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item, directions);
+        spinner.setAdapter(routeAdapter);
+    }
+
+    private void updateStops(Spinner spinner, String route, String direction) {
+        ArrayList<String> stops = routeInfo.getStopsForRouteDirection(route, direction);
+        if (stops == null) {
+            stops = new ArrayList<>(0);
+            spinner.setEnabled(false);
+        } else {
+            spinner.setEnabled(true);
+        }
+        ArrayAdapter<String> routeAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item, stops);
+        spinner.setAdapter(routeAdapter);
     }
 
     public void startNewTrip(View view) {
